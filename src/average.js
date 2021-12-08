d6ce3ffcb3be303d6bf83eafab2539f2a0b8d36d
Allana Karla <allanakarla1702@gmail.com>
2021-12-08 17:01:05 -0300

    Erros Lint

--- src/average.js
+++ src/average.js
@@ -27,6 +27,5 @@ const average = (arrNum) => {
   /* Refêrencia Math.round- https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round */
    return Math.round(result);
   };
-  //console.log(average([]));
   
 module.exports = average;
