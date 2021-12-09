a412a996d539bc19fb5456eff3a3f0a18395206d
Allana Karla <allanakarla1702@gmail.com>
2021-12-08 22:24:35 -0300

    Requisito 3 - correcao template literals

--- src/vqv.js
+++ src/vqv.js
@@ -18,10 +18,10 @@
 
 const vqv = (name, age) => {
   if (typeof name === 'string' && typeof age === 'number') {
-    return `Oi, meu nome é ${name}!
-    Tenho ${age} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   } 
   return undefined;
 };
